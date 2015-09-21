323a9faa9091d8adc6e101ea66a5e0e3efd963ec
Felix Itzenplitz <felix@stkn.org>
2015-09-21 16:55:27 +0200

    Initial commit

---
+++ common/models/metering.js
@@ -0,0 +1,22 @@
+module.exports = function (Metering) {
+  Metering.validateAsync('time', timeValidator, {
+    message: 'Bad Time'
+  });
+
+  function timeValidator(err, done) {
+    var self = this;
+    Metering.findOne({
+        where: {
+          'sensorId': self.sensorId
+        },
+        order: 'time DESC'
+      },
+      function (e, metering) {
+        if (metering && metering.time >= self.time) {
+          err();
+        }
+        done();
+      }
+    );
+  }
+};
